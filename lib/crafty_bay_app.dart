5286757c735c7fb3b93148fec7d2071ed3bc13d0
bidhannath2001 <bidhannath2001@gmail.com>
2026-02-17 02:31:02 +0600

    firebase setup

---
+++ lib/crafty_bay_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatelessWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
